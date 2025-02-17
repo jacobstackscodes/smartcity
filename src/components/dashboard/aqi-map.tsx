c9eb002d0d8bc2e39226d52627a84abac0fab56b
Nathanael DCunha <nathanael.dcunha@gmail.com>
2025-02-17 15:19:58 +0530

    chore: refactored map coomponents

--- src/components/dashboard/aqi-map.tsx
+++ src/components/dashboard/aqi-map.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { mapStyles } from '@/lib/constants';
-import { Loader } from '@googlemaps/js-api-loader';
-import { useEffect, useMemo, useRef } from 'react';
+import { AdvancedMarker, Map } from '../ui/maps';
 
 type Props = {
     center?: {
@@ -11,72 +9,15 @@ type Props = {
     };
 };
 
-const mapContainerStyle = {
-    width: '100%',
-    height: '100%',
-};
-
 export const AQIMap = ({
     center: _center = {
         lat: 12.9716,
         lng: 77.5946,
     },
 }: Props) => {
-    const center = useMemo(() => _center, [_center]);
-    const mapRef = useRef<HTMLDivElement | null>(null);
-    const mapInstanceRef = useRef<google.maps.Map | null>(null);
-    const markerRef = useRef<google.maps.marker.AdvancedMarkerElement | null>(
-        null,
+    return (
+        <Map>
+            <AdvancedMarker position={_center} />
+        </Map>
     );
-
-    useEffect(() => {
-        const initMap = async () => {
-            const loader = new Loader({
-                id: 'google-maps-script',
-                apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
-            });
-
-            const { Map } = await loader.importLibrary('maps');
-            const { AdvancedMarkerElement } =
-                await loader.importLibrary('marker');
-
-            if (mapRef.current && !mapInstanceRef.current) {
-                const map = new Map(mapRef.current, {
-                    center,
-                    zoom: 14,
-                    mapTypeControl: false,
-                    streetViewControl: false,
-                    draggable: true,
-                    scrollwheel: true,
-                    panControl: false,
-                    mapId: 'react-advanced-maps',
-                    styles: mapStyles,
-                });
-
-                const marker = new AdvancedMarkerElement({
-                    position: center,
-                    map,
-                });
-
-                mapInstanceRef.current = map;
-                markerRef.current = marker;
-            }
-        };
-
-        initMap();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    // Pan to new center when `center` updates
-    useEffect(() => {
-        if (mapInstanceRef.current) {
-            mapInstanceRef.current.panTo(center);
-        }
-
-        if (markerRef.current) {
-            markerRef.current.position = center;
-        }
-    }, [center]);
-
-    return <div ref={mapRef} style={mapContainerStyle} />;
 };
---
+++ src/components/ui/maps.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { Loader } from '@googlemaps/js-api-loader';
+import { useEffect, useRef, useMemo } from 'react';
+import { create } from 'zustand';
+
+interface MapStore {
+    map: google.maps.Map | null;
+    mapRef: React.RefObject<HTMLDivElement | null>;
+    setMap: (map: google.maps.Map) => void;
+}
+
+const useMapStore = create<MapStore>((set) => ({
+    map: null,
+    mapRef: { current: null },
+    setMap: (map) => set({ map }),
+}));
+
+const loader = new Loader({
+    id: 'google-maps-script',
+    apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+});
+
+const Map: React.FC<{
+    children?: React.ReactNode;
+}> = (props) => {
+    const { setMap, mapRef } = useMapStore();
+    const localMapRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!mapRef.current) {
+            mapRef.current = localMapRef.current;
+        }
+    }, [mapRef]);
+
+    const initializeMap = useMemo(() => {
+        return async () => {
+            if (!mapRef.current) return;
+            const { Map } = await loader.importLibrary('maps');
+            const _map = new Map(mapRef.current, {
+                center: { lat: 12.9716, lng: 77.5946 },
+                zoom: 14,
+                mapTypeControl: false,
+                streetViewControl: false,
+                zoomControl: false,
+                draggable: false,
+                scrollwheel: false,
+                panControl: false,
+                mapId: '67af307b850dc59d',
+            });
+            setMap(_map);
+        };
+    }, [setMap, mapRef]);
+
+    useEffect(() => {
+        initializeMap();
+    }, [initializeMap]);
+
+    return (
+        <div
+            ref={localMapRef}
+            style={{ height: '100%', width: '100%' }}
+            {...props}
+        />
+    );
+};
+
+const AdvancedMarker: React.FC<{
+    position: { lat: number; lng: number };
+}> = ({ position }) => {
+    const { map } = useMapStore();
+
+    useEffect(() => {
+        const initializeMarker = async () => {
+            if (!map) return;
+            const { AdvancedMarkerElement } =
+                await loader.importLibrary('marker');
+            new AdvancedMarkerElement({
+                position: position ?? { lat: 12.9716, lng: 77.5946 },
+                map: map,
+                title: 'Bangalore',
+            });
+        };
+
+        initializeMarker();
+    }, [map, position]);
+
+    return null;
+};
+
+export { Map, AdvancedMarker };
