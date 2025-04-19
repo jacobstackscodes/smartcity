54eb2e587e6a7ebf0b7ac87854275700fd880046
Nathanael DCunha <71888935+nathanaeld01@users.noreply.github.com>
2025-04-19 20:07:14 +0530

    insights redo

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
     title: 'Insights',
 };
 
-export default function Page() {
+export default async function Page() {
     return (
         <div className="wrapper min-h-screen border-x border-border bg-white pt-16">
             <main className="w-full py-8"></main>
